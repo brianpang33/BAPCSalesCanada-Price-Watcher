7899976f73e480f2ff5674c371a448d30cff04f3
Brian <brianpang221@gmail.com>
2020-01-02 17:28:09 -0800

    delete functionality and render items onto popup

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ $(function () {
     });
 
     // process saving the value and displaying it on popup
-    $('#Submit').click(function () {
+    $('#Add').click(function () {
         let userInput = $('#Input').find(":selected").text();
         if (userInput) {
             window.watchList = new Set(Array.from(window.watchList));
@@ -24,13 +24,78 @@ $(function () {
             renderItems(window.watchList);
         });
     });
+
+    $('#Delete').click(function() {
+        let userInput = $('#Input').find(":selected").text();
+        chrome.storage.sync.get('watchList', function (result) {
+            let index = Array.from(result.watchList).indexOf(userInput);
+            if (index > -1) {
+                result.watchList.splice(index, 1);
+                window.watchList = result.watchList;
+                chrome.storage.sync.set({ 'watchList': window.watchList });
+                renderItems(window.watchList);
+            }
+        });
+    });
 });
 
 
 function renderItems(watchList) {
-    //TODO
+    $('#itemList').empty();
+    for (let i = 0; i < watchList.length; i++) {
+        renderList(watchList[i]).then(function(result) {
+            if (result !== undefined) {
+                $('#itemList').append(
+                    '<div class="card">' +
+                        '<div class="card-header" id="heading' + i + '">' +
+                            '<h5 class="mb-0">' +
+                                '<button class="btn btn-link" data-toggle="collapse" data-target="#collapse' + i + '" aria-expanded="true" aria-controls="collapse' + i + '">' +
+                                    watchList[i] +
+                                '</button>' +
+                             '</h5>' +
+                        '</div>' +
+
+                        '<div id="collapse' + i + '" class="panel-collapse collapse in" aria-labelledby="heading' + i + '" data-parent="#itemList">' +
+                            '<ul class="list-group">' + result +
+                            '</div>' +
+                        '</div>' +
+                    '</div>'
+                )
+            } else {
+                $('#itemList').append(
+                    '<div class="card">' +
+                        '<div class="card-header" id="heading' + i + '">' +
+                            '<h5 class="mb-0">' +
+                                '<button class="btn btn-link" data-toggle="collapse" data-target="#collapse' + i + '" aria-expanded="true" aria-controls="collapse' + i + '">' +
+                                    watchList[i] +
+                                '</button>' +
+                            '</h5>' +
+                        '</div>' +
+                    '</div>'
+                )
+            }
+        });
+    }
 }
 
 function renderList(key) {
-    //TODO
+    return new Promise(function (resolve, reject) {
+        chrome.storage.sync.get('data', function (result) {
+            if (result.data) {
+                let items = result.data;
+                if (items[key].length > 0) {
+                    let resultString = "";
+                    for (item of items[key]) {
+                        resultString +=              
+                        '<a href="' + item["url"] + '" class ="list-group-item" target="_blank">' + item["title"] + '</a>'
+                    }
+                    resolve(resultString);
+                } else {
+                    resolve(undefined);
+                }
+            } else {
+                resolve(undefined);
+            }
+        });
+    });
 }
